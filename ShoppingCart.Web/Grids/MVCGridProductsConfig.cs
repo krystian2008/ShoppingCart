c10fa10b5576763dd7b98cb6898ce90cb5cde663
Krystian Polak <krystian.polak@live.com>
2017-10-23 13:44:00 +0200

    refactor mvcgrid configuration

--- ShoppingCart.Web/Grids/MVCGridProductsConfig.cs
+++ ShoppingCart.Web/Grids/MVCGridProductsConfig.cs
@@ -13,6 +13,8 @@ namespace ShoppingCart.Web.Grids
     public static class MVCGridProductsConfig
     {
         private const string GRID_NAME = "ProductsMVCGrid";
+        private const string FILTER_NAME = "filterName";
+        private const string FILTER_DESC = "filterDesc";
 
         public static void RegisterGrid(GridDefaults gridDefaults, ColumnDefaults colDefauls)
         {
@@ -25,100 +27,21 @@ namespace ShoppingCart.Web.Grids
                 .WithAuthorizationType(AuthorizationType.AllowAnonymous)
                 .AddColumns(cols =>
                 {
-                    cols.Add().WithColumnName("Identifier")
-                        .WithSorting(true)
-                        .WithHeaderText("Identifier")
-                        .WithValueExpression(i => i.Id.ToString())
-                        .WithVisibility(false);
-                    cols.Add().WithColumnName("Name")
-                        .WithSorting(true)
-                        .WithFiltering(true)
-                        .WithHeaderText("Name")
-                        .WithValueExpression(i => i.Name);
-                    cols.Add().WithColumnName("Description")
-                        .WithSorting(true)
-                        .WithFiltering(true)
-                        .WithHeaderText("Description")
-                        .WithValueExpression(i => i.Description);
-                    cols.Add().WithColumnName("Price")
-                        .WithSorting(true)
-                        .WithFiltering(true)
-                        .WithHeaderText("Price")
-                        .WithValueExpression(i => i.Price.ToString());
-                    cols.Add().WithColumnName("Stock")
-                        .WithSorting(true)
-                        .WithFiltering(true)
-                        .WithHeaderText("Stock")
-                        .WithValueExpression(i => i.Stock.ToString());
-                    cols.Add("Edit").WithHtmlEncoding(false)
-                    .WithSorting(false)
-                    .WithHeaderText(" ")
-                    .WithValueExpression((p, c) => c.UrlHelper.Action("AddToCart", "Products", new { productId = p.Id }))
-                    .WithValueTemplate("<a href='{Value}' class='btn btn-primary' role='button' title='future use'>Add To Cart</a>");
+                    BuildColumns(cols);
                 })
                 .WithSorting(true, "Name", SortDirection.Asc)
                 .WithFiltering(true)
-                .WithAdditionalQueryOptionNames("filterName", "filterDesc")
+                .WithAdditionalQueryOptionNames(FILTER_NAME, FILTER_DESC)
                 .WithRetrieveDataMethod((context) =>
                 {
                     var service = DependencyResolver.Current.GetService<IProductsService>();
                     var result = service.GetProducts();
                     var options = context.QueryOptions;
                     var query = result.Items.AsQueryable();
-                    var name = options.GetAdditionalQueryOptionString("filterName");
-                    var desc = options.GetAdditionalQueryOptionString("filterDesc");
+                    var name = options.GetAdditionalQueryOptionString(FILTER_NAME);
+                    var desc = options.GetAdditionalQueryOptionString(FILTER_DESC);
 
-                    if (!string.IsNullOrWhiteSpace(name))
-                    {
-                        query = query.Where(x => x.Name.ToLower().Contains(name.ToLower()));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(desc))
-                    {
-                        query = query.Where(x => x.Description.ToLower().Contains(desc.ToLower()));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(options.SortColumnName))
-                    {
-                        switch (options.SortColumnName.ToLower())
-                        {
-                            case "identifier":
-                                if (options.SortDirection == SortDirection.Asc)
-                                    query = query.OrderBy(p => p.Id);
-                                else
-                                    query = query.OrderByDescending(p => p.Id);
-                                break;
-                            case "name":
-                                if (options.SortDirection == SortDirection.Asc)
-                                    query = query.OrderBy(p => p.Name);
-                                else
-                                    query = query.OrderByDescending(p => p.Name);
-                                break;
-                            case "description":
-                                if (options.SortDirection == SortDirection.Asc)
-                                    query = query.OrderBy(p => p.Description);
-                                else
-                                    query = query.OrderByDescending(p => p.Description);
-                                break;
-                            case "price":
-                                if (options.SortDirection == SortDirection.Asc)
-                                    query = query.OrderBy(p => p.Price);
-                                else
-                                    query = query.OrderByDescending(p => p.Price);
-                                break;
-                            case "stock":
-                                if (options.SortDirection == SortDirection.Asc)
-                                    query = query.OrderBy(p => p.Stock);
-                                else
-                                    query = query.OrderByDescending(p => p.Stock);
-                                break;
-                        }
-                    }
-
-                    if (options.GetLimitOffset().HasValue)
-                    {
-                        query = query.Skip(options.GetLimitOffset().Value).Take(options.GetLimitRowcount().Value);
-                    }
+                    query = FilterData(options, query, name, desc);                    
 
                     return new QueryResult<ProductItemModel>()
                     {
@@ -127,6 +50,97 @@ namespace ShoppingCart.Web.Grids
                     };
                 })
             );
+        }
+
+        private static IQueryable<ProductItemModel> FilterData(QueryOptions options, IQueryable<ProductItemModel> query, string name, string desc)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                query = query.Where(x => x.Name.ToLower().Contains(name.ToLower()));
+            }
+
+            if (!string.IsNullOrWhiteSpace(desc))
+            {
+                query = query.Where(x => x.Description.ToLower().Contains(desc.ToLower()));
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.SortColumnName))
+            {
+                switch (options.SortColumnName.ToLower())
+                {
+                    case "identifier":
+                        if (options.SortDirection == SortDirection.Asc)
+                            query = query.OrderBy(p => p.Id);
+                        else
+                            query = query.OrderByDescending(p => p.Id);
+                        break;
+                    case "name":
+                        if (options.SortDirection == SortDirection.Asc)
+                            query = query.OrderBy(p => p.Name);
+                        else
+                            query = query.OrderByDescending(p => p.Name);
+                        break;
+                    case "description":
+                        if (options.SortDirection == SortDirection.Asc)
+                            query = query.OrderBy(p => p.Description);
+                        else
+                            query = query.OrderByDescending(p => p.Description);
+                        break;
+                    case "price":
+                        if (options.SortDirection == SortDirection.Asc)
+                            query = query.OrderBy(p => p.Price);
+                        else
+                            query = query.OrderByDescending(p => p.Price);
+                        break;
+                    case "stock":
+                        if (options.SortDirection == SortDirection.Asc)
+                            query = query.OrderBy(p => p.Stock);
+                        else
+                            query = query.OrderByDescending(p => p.Stock);
+                        break;
+                }
+            }
+
+            if (options.GetLimitOffset().HasValue)
+            {
+                query = query.Skip(options.GetLimitOffset().Value).Take(options.GetLimitRowcount().Value);
+            }
+
+            return query;
+        }
+
+        private static void BuildColumns(GridColumnListBuilder<ProductItemModel> cols)
+        {
+            cols.Add().WithColumnName("Identifier")
+                                    .WithSorting(true)
+                                    .WithHeaderText("Identifier")
+                                    .WithValueExpression(i => i.Id.ToString())
+                                    .WithVisibility(false);
+            cols.Add().WithColumnName("Name")
+                .WithSorting(true)
+                .WithFiltering(true)
+                .WithHeaderText("Name")
+                .WithValueExpression(i => i.Name);
+            cols.Add().WithColumnName("Description")
+                .WithSorting(true)
+                .WithFiltering(true)
+                .WithHeaderText("Description")
+                .WithValueExpression(i => i.Description);
+            cols.Add().WithColumnName("Price")
+                .WithSorting(true)
+                .WithFiltering(true)
+                .WithHeaderText("Price")
+                .WithValueExpression(i => i.Price.ToString());
+            cols.Add().WithColumnName("Stock")
+                .WithSorting(true)
+                .WithFiltering(true)
+                .WithHeaderText("Stock")
+                .WithValueExpression(i => i.Stock.ToString());
+            cols.Add("Edit").WithHtmlEncoding(false)
+            .WithSorting(false)
+            .WithHeaderText(" ")
+            .WithValueExpression((p, c) => c.UrlHelper.Action("AddToCart", "Products", new { productId = p.Id }))
+            .WithValueTemplate("<a href='{Value}' class='btn btn-primary' role='button' title='future use'>Add To Cart</a>");
         }
     }
 }
